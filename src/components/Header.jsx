9adae98cc939688b3c432fc0fa57cc81d15415aa
Robson Narcizo <rob.narcizo.27@gmail.com>
2022-11-03 16:44:21 -0300

    Estilização Titulo e Icones Cabeçalho

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React, { useContext } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
+import { BiDish } from 'react-icons/bi';
+import { GiKnifeFork } from 'react-icons/gi';
 import AppContext from '../context/AppContext';
 import profile from '../images/profileIcon.svg';
 import search from '../images/searchIcon.svg';
 import SearchBar from './SearchBar';
+import '../styles/Header.css';
 
 function Header() {
   const history = useHistory();
@@ -19,34 +22,46 @@ function Header() {
 
   return (
     <header>
+      <div className="header">
+        <div className="logo">
+          <BiDish
+            style={ { height: '60px', width: '60px' } }
+          />
+        </div>
+        <p>JSON.foodify()</p>
+        <div className="container-search-icon">
+          <button
+            type="button"
+            data-testid="profile-top-btn"
+            src={ profile }
+            onClick={ handleClick }
+          >
+            <img src={ profile } alt="profile-button" />
+
+          </button>
+          {pages.includes(getTitle()) ? null : (
+            <button
+              type="button"
+              data-testid="search-top-btn"
+              src={ search }
+              onClick={ () => setSearchInput((prevState) => !prevState) }
+            >
+              <img src={ search } alt="search-button" />
+
+            </button>
+          )}
+        </div>
+      </div>
+      <p className="fork-icon">
+        <GiKnifeFork
+          style={ { fontSize: '3em', margin: '0 auto' } }
+        />
+      </p>
       <h1
         data-testid="page-title"
       >
         {getTitle(id)}
       </h1>
-
-      <button
-        type="button"
-        data-testid="profile-top-btn"
-        src={ profile }
-        onClick={ handleClick }
-        style={ { height: '35px', width: '35px' } }
-      >
-        <img src={ profile } alt="profile-button" />
-
-      </button>
-      {pages.includes(getTitle()) ? null : (
-        <button
-          type="button"
-          data-testid="search-top-btn"
-          src={ search }
-          onClick={ () => setSearchInput((prevState) => !prevState) }
-          style={ { height: '35px', width: '35px' } }
-        >
-          <img src={ search } alt="search-button" />
-
-        </button>
-      )}
       {searchInput && <SearchBar />}
     </header>
   );
