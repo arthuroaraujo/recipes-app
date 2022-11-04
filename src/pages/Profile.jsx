91780fa04ae45d0c85f20730cc39a62afc8b2544
Arthur <arthuroaraujo@gmail.com>
2022-11-04 12:17:58 -0300

    Corrigindo css e página de profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import MainContent from '../components/MainContent';
+import AppContext from '../context/AppContext';
 import '../styles/Profile.css';
 
 function Profile() {
+  const { setSearchInput } = useContext(AppContext);
+  useEffect(() => {
+    setSearchInput(false);
+  }, []);
+
   return (
     <>
       <Header />
